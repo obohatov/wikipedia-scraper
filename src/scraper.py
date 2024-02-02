cf24a0f3f3d1df87d7eec2ba6391f8e8a535c902
Oleh Bohatov <obohatov@gmail.com>
2024-02-02 09:50:01 +0100

    add some methods to scraper

--- src/scraper.py
+++ src/scraper.py
@@ -6,10 +6,10 @@ import re
 
 class WikipediaScraper:
     def __init__(self):
-        self.base_url = 'https://country-leaders.onrender.com'
-        self.country_endpoint = '/countries'
-        self.leaders_endpoint = '/leaders'
-        self.cookies_endpoint = '/cookie'
+        self.base_url = "https://country-leaders.onrender.com"
+        self.country_endpoint = "/countries"
+        self.leaders_endpoint = "/leaders"
+        self.cookies_endpoint = "/cookie"
         self.leaders_data = {}
 
     def refresh_cookie(self):
@@ -25,11 +25,26 @@ class WikipediaScraper:
         return countries
 
     def get_leaders(self, country):
-        pass
+        leaders_url = self.base_url + self.leaders_endpoint
+        response = requests.get(leaders_url, params={"country": country}, cookies=self.cookie)
+        leaders = response.json()
+        self.leaders_data[country] = leaders
 
     def get_first_paragraph(self, wikipedia_url):
-        pass
+        response_wiki = requests.get(wikipedia_url)
+        soup = BeautifulSoup(response_wiki.text, "html.parser")
+
+        bodyContent = soup.find("div", id="bodyContent")
+        paragraphs = bodyContent.find_all("p")
+        first_paragraph = ""
+        for paragraph in paragraphs:
+            if paragraph.find("b"):
+                first_paragraph = paragraph.text
+                break
+
+        first_paragraph = re.sub(r"\{.*?\}|<.*?>|\[.*?\]", "", first_paragraph)
+        return first_paragraph
 
     def to_json_file(self, filepath):
-        pass
-    
+        with open(filepath, "w") as json_file:
+            json.dump(self.leaders_data, json_file)
